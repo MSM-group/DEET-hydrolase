37ea5d6b36541aacfc0e2c639e352cacaee7d715
Serina Robinson <robinsonserinalee@gmail.com>
2024-03-31 21:58:18 +0200

    alpha helix analysis

--- scripts/Figure2b.r
+++ scripts/Figure2b.r
@@ -1,4 +1,5 @@
-rm(list = ls())
+# Load packages
+# Install packages if not already installed
 library(tidyverse)
 library(treedataverse)
 library(RColorBrewer)
@@ -8,7 +9,9 @@ library(ggstar)
 library(ggtree)
 
 # Set color palette and define functions
-color_palette <- read_csv("data/taxonomy/taxonomic_phylum_color_palette_original.csv")
+color_palette <- read_csv("data/taxonomy/taxonomic_phylum_color_palette_original.csv") %>%
+  bind_rows(data.frame(hexcode = "gray80", phylum = ""))
+
 rename_seq_lab <- function(name){
   dplyr::case_when(str_detect(name, "k121")|str_detect(name, "DEET")|str_detect(name, "Cocaine") ~ name,
                    str_detect(name, "WP") ~ str_remove(name, "\\."),
@@ -68,51 +71,66 @@ assay_res <- read_rds("data/enzyme_activity_data/assay_tree_data.rds")
 metadat <- read_excel("data/taxonomy/table_s2_original.xlsx", skip = 1) %>%
   mutate(seqid = word(name, sep = "_", start = 1, end = 3)) %>%
   select(seqid, phylum)
+metadat$phylum[metadat$phylum == "Verrucomicrobia"] <- "Verrucomicrobiota"
 
 # Sequence-based tree
 seq_based_tree <- ape::read.tree("data/phylogeny/DH_tree_bootstrap_100.treefile") %>%
   treeio::as_tibble() %>%
-  mutate(label = rename_seq_lab(label)) %>%
-  left_join(metadat, by = c("label" = "seqid")) %>%
-  left_join(p_labels, by = c("label" = "seqid")) %>%
-  left_join(assay_res, by = c("p_name" = "enzyme")) %>%
-  mutate(phylum = add_phylum(label, phylum)) %>%
-  lab = generate_tip_labs(label, p_name, diethyltoluamide, methylbenzoate, x4np_butyrate, x4np_trimethylacetate) %>%
+  dplyr::mutate(label = rename_seq_lab(label)) %>%
+  left_join(., metadat, by = c("label" = "seqid")) %>%
+  left_join(., p_labels, by = c("label" = "seqid")) %>%
+  left_join(., assay_res, by = c("p_name" = "id")) %>%
+  dplyr::mutate(phylum = add_phylum(label, phylum)) %>%
+  dplyr::mutate(lab = generate_tip_labs(label, p_name, diethyltulouamide, methylbenzoate, x4np_butyrate, x4np_trimethylacetate)) %>%
   treeio::as.treedata()
+seq_based_tree_plot <- ggtree::ggtree(seq_based_tree)
 
 # Structure-based tree
-struc_based_tree <- readr::read_rds("data/phylogeny/rmsd_tree.rds") %>%
-  ape::root(outgroup = c("6QHV", "1MJ5", "3HI4"))
-struc_based_tree_plot <- ggtree::ggtree(struc_based_tree) +
-  geom_tiplab(size = 1.5)
+rmsd <- readr::read_rds("data/phylogeny/new_struc_dist.rds")
+hc <- nj(rmsd)
+tree <- hc %>% as.treedata()
+
+struc_based_tree_no_outgroup <- ggtree::ggtree(tree)
+struc_based_tree_no_outgroup
+struc_based_tree_no_outgroup$data$label
+struc_based_tree <- ggtree(ape::root(phy = tree, outgroup = c("6QHV", "1MJ5", "3HI4"))) #edgelabel = T)) #resolve.root = TRUE))
+
+struc_based_tree$data$label <- c(struc_based_tree_no_outgroup$data$label)
+struc_based_tree$data
+
+
 dat_seq <- seq_based_tree_plot$data %>%
-  dplyr::mutate(color_deet_star = get_star_color(diethyltoluamide),
+  dplyr::mutate(color_deet_star = get_star_color(diethyltulouamide),
                 color_methylbenzoate_star = get_star_color(methylbenzoate),
                 color_4np_butyrate_star = get_star_color(x4np_butyrate),
                 color_4np_trimethylacetate_star = get_star_color(x4np_trimethylacetate),
-                x_deet_star = get_deet_star(diethyltoluamide, x),
-                x_methylbenzoate_star = get_methylbenzoate_star_x(diethyltoluamide, methylbenzoate, x),
-                x_4np_butyrate_star = get_4np_butyrate_star_x(diethyltoluamide, methylbenzoate, x4np_butyrate, x),
-                x_4np_trimethylacetate_star = get_4np_trimethylacetate_star_x(diethyltoluamide, methylbenzoate, x4np_butyrate, x4np_trimethylacetate, x),
+                x_deet_star = get_deet_star(diethyltulouamide, x),
+                x_methylbenzoate_star = get_methylbenzoate_star_x(diethyltulouamide, methylbenzoate, x),
+                x_4np_butyrate_star = get_4np_butyrate_star_x(diethyltulouamide, methylbenzoate, x4np_butyrate, x),
+                x_4np_trimethylacetate_star = get_4np_trimethylacetate_star_x(diethyltulouamide, methylbenzoate, x4np_butyrate, x4np_trimethylacetate, x),
                 x_tip_lab_with_star = case_when(lab == "Cocaine esterase" ~ x + 0.3,
                                                 TRUE ~ pmax(x_deet_star, x_methylbenzoate_star, x_4np_butyrate_star, x_4np_trimethylacetate_star, na.rm = TRUE)+0.3))
-dat_struc <- struc_based_tree_plot$data %>%
+
+dat_struc <- struc_based_tree_no_outgroup$data %>%
   mutate(x = max(x) - x + max(dat_seq$x) + 5) %>%
-  left_join(metadat, by = c("label" = "seqid")) %>%
-  left_join(p_labels, by = c("label" = "seqid")) %>%
-  left_join(assay_res, by = c("p_name" = "id")) %>%
-  mutate(phylum = add_phylum(label, phylum),
-         lab = generate_tip_labs(label, p_name, diethyltoluamide, methylbenzoate, x4np_butyrate, x4np_trimethylacetate))
+  left_join(., metadat, by = c("label" = "seqid")) %>%
+  left_join(., p_labels, by = c("label" = "seqid")) %>%
+  left_join(., assay_res, by = c("p_name" = "id")) %>%
+  dplyr::mutate(phylum = add_phylum(label, phylum),
+         lab = generate_tip_labs(label, p_name, diethyltulouamide, methylbenzoate, x4np_butyrate, x4np_trimethylacetate))
 dat_combined <- bind_rows(dplyr::select(dat_seq, !ends_with("star")), dat_struc) %>%
   tidyr::drop_na(label) %>%
   filter(parent != node)
+
 struc_based_tree_plot_2 <- ggtree::ggtree(dat_struc, color = "gray")
+
 combined_tree <- struc_based_tree_plot_2 +
+  geom_nodelab() +
   geom_tree(dat_seq, color = "gray") +
-  geom_line(aes(x = x, y = y, group = label, color = phylum), data = filter(dat_combined, isTip == TRUE), linewidth = 0.3, alpha = 0.5) +
+  geom_line(aes(x = x, y = y, group = label, color = phylum), data = filter(dat_combined, isTip == TRUE), linewidth = 0.5, alpha = 0.5) +
   geom_tiplab(aes(label = lab), size = 2.5, hjust = 1, offset = -0.1) +
   geom_tiplab(data = dat_seq, aes(x = x_tip_lab_with_star, label = lab), size = 2.5) +
-  geom_point(data = drop_na(dat_combined, phylum), aes(x = x, y = y, color = phylum), size = 1) +
+  geom_point(data = drop_na(dat_combined, phylum), aes(x = x, y = y, color = phylum), size = 1, shape = 15) +
   scale_color_manual(name = "Phylum", values = pull(color_palette, hexcode)) +
   ggnewscale::new_scale_color() +
   geom_nodepoint(data = filter(dat_seq, isTip == FALSE & parent != node), aes(color = as.numeric(label)), size = 1) +
@@ -135,7 +153,7 @@ ggplot2::ggsave(combined_tree,
                 dpi = 320,
                 device = "png",
                 units = "in",
-                width = 7,
+                width = 12,
                 height = 5)
 
 
---
+++ scripts/helix_6_7_analysis.py
@@ -0,0 +1,179 @@
+
+
+def find_closest_res_to_reference(prot1,prot2):
+
+    # Initialize a list to store the indices of the closest residues
+    closest_residues = []
+    
+    # Define the residues of interest in protein1
+    residues_of_interest = [213, 251]
+    
+    # For each residue of interest, find the closest residue in protein2
+    for resi in residues_of_interest:
+        # Create a selection for the C-alpha atom of the residue of interest
+        cmd.select('resi_interest', f'{prot1} and resi {resi} and name CA')
+    
+        # Get the list of all residues in protein2
+        residues_in_protein2 = cmd.get_model(f'{prot2} and name CA').atom
+    
+        # Initialize the minimum distance to a large number
+        min_distance = 9999
+        nearest_resi = None
+        nearest_resn = None
+    
+        # Iterate over all residues in protein2
+        for atom in residues_in_protein2:
+            # Calculate the distance between the current atom and the residue of interest
+            distance = cmd.get_distance('resi_interest', f'{prot2} and resi {atom.resi} and name CA')
+    
+            # If the calculated distance is less than the minimum distance, update the minimum distance and the nearest residue
+            if distance < min_distance:
+                min_distance = distance
+                nearest_resi = atom.resi
+                nearest_resn = atom.resn
+    
+        # Print the result
+        print(f'The closest residue to residue {resi} in {prot1} is residue {nearest_resn} {nearest_resi} in {prot2}')
+    
+        # Add the index of the closest residue to the list
+        closest_residues.append(int(nearest_resi))
+        
+        return(closest_residues)
+
+def get_alpha_helix_string(molecule, closest_residues, num_zeros):
+    # Assign secondary structure
+    cmd.dss(molecule)
+
+    # Initialize the result string
+    result = ""
+
+    # Define a function to be called for each residue
+    def check_residue(residue_name, ss, resi):
+        nonlocal result
+        if ss == 'H':
+            result += '1'
+        else:
+            result += '0'
+
+        # If the current residue is one of the closest residues, change the character to '2'
+        if int(resi) in closest_residues:
+            result = result[:-1] + '2'
+
+    # Iterate over each residue
+    cmd.iterate(f'{molecule} and name ca', 'check_residue(resn, ss, resi)', space=locals())
+
+    # Store the original result
+    original_result = result
+
+    # Treat '2' as '1'
+    result = result.replace('2', '1')
+
+    # Check for '1's and change '0's to '1's if there are fewer than num_zeros '0's before and after
+    result_list = list(result)
+    for i in range(len(result_list)):
+        if result_list[i] == '1':
+            # Find the start and end of the cluster of '1's
+            start = i
+            while start > 0 and result_list[start - 1] == '1':
+                start -= 1
+            end = i
+            while end < len(result_list) - 1 and result_list[end + 1] == '1':
+                end += 1
+
+            # Check the number of '0's before the cluster
+            num_zeros_before = 0
+            for j in range(start - 1, -1, -1):
+                if result_list[j] == '0':
+                    num_zeros_before += 1
+                else:
+                    break
+
+            # Check the number of '0's after the cluster
+            num_zeros_after = 0
+            for j in range(end + 1, len(result_list)):
+                if result_list[j] == '0':
+                    num_zeros_after += 1
+                else:
+                    break
+
+            # Change '0's to '1's if there are fewer than num_zeros '0's before and after the cluster
+            if num_zeros_before < num_zeros:
+                for j in range(start - num_zeros_before, start):
+                    result_list[j] = '1'
+            if num_zeros_after < num_zeros:
+                for j in range(end + 1, end + 1 + num_zeros_after):
+                    result_list[j] = '1'
+
+    result = ''.join(result_list)
+
+    # Change '2's back to '2'
+    for i in range(len(result)):
+        if original_result[i] == '2':
+            result = result[:i] + '2' + result[i+1:]
+
+    # Get the residue sequence of prot2
+    residue_sequence = cmd.get_fastastr(molecule).split('\n', 1)[1].replace('\n', '')
+
+    # Find clusters of '1's that contain a '2' and count how many '1's and '2's they contain
+    helix_lengths = []
+    i = 0
+    while i < len(result):
+        if result[i] == '2':
+            start = i
+            while start > 0 and result[start - 1] == '1':
+                start -= 1
+            end = i
+            while end < len(result) - 1 and result[end + 1] == '1':
+                end += 1
+            helix_lengths.append(end - start + 1)
+            i = end + 1
+        else:
+            i += 1
+
+    # Write the results to a text file
+    with open('Result_Helixes.txt', 'a') as f:
+        f.write(f'{molecule}\n')
+        f.write(f'{residue_sequence}\n')
+        f.write(f'{original_result}\n')
+        f.write(f'{result}\n')
+        f.write(f'{helix_lengths}\n')
+
+
+import pymol
+from pymol import cmd
+import os
+
+
+
+
+
+def process_folder(folder_path):
+    # Change to the specified folder
+    os.chdir(folder_path)
+
+    # Get a list of all .pdb files in the folder
+    pdb_files = [f for f in os.listdir() if f.endswith(".pdb")]
+
+    for pdb_file in pdb_files:
+        # Load the PDB file and assign the name 'prot1'
+        cmd.load(pdb_file, 'prot2')
+        cmd.load(r"\\eawag\userdata\felderfl\Desktop\alpha_helix_test\Reference.pdb",'prot1')
+        
+        prot1='prot1'
+        prot2='prot2'
+        
+        cmd.super(prot1, prot2)
+        
+        get_alpha_helix_string(prot2, find_closest_res_to_reference(prot1,prot2), 6)
+        
+        
+        # Reinitialize Pymol
+
+        cmd.reinitialize()
+
+
+folder_path = r'\\eawag\userdata\felderfl\Desktop\alpha_helix_test\pdb_folder'
+
+# Call the function to process the folder
+process_folder(folder_path)
+
